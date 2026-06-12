f9fd28a7616f43e7c30cf39b296b3fee43653837
iledgerlpg <iledgerlpg@gmail.com>
2026-06-12 13:44:10 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 
 const CONFIG = {
 
-  APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyo7TijrKlKakbB0xCXwNTL1zPhg_6tgO3ij-J4eJgOXJiLYLue66N01sdv3uDHXTyj/exec',
+  APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwzXbLWCXxlQDINl-0OcEknu-5fBCfRLWnnxvYU1zTg7X4KhU_G79R57Bm9HoyAHVRd/exec',
 
   APP_NAME: 'iLedgerV2',
   APP_VERSION: '2.0.0',
